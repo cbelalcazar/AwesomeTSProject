243b4e3f58b052ae5387a5102917afbe640a0876
Carlos Belalcazar <cbelalcazar@EN2010522.local>
2021-09-30 10:17:55 -0500

    first commit

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text
+        style={{
+          color: 'white',
+          fontSize: 200,
+        }}>
+        Hola Mundo!!!
+      </Text>
+    </View>
+  );
+};
+
+export default App;
